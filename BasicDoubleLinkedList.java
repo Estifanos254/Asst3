28a00c3c2253506130a8c6ba71dfbf66cfa507bd
Estifanos Kebebew <65937750+Estifanos254@users.noreply.github.com>
2023-03-17 19:22:11 -0400

    Add files via upload
    Please see these files

---
+++ BasicDoubleLinkedList.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class BasicDoubleLinkedList<T> {
+
+	public void addToFront(String newElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ArrayList<String> toArrayList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addToEnd(String newElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String retrieveFirstElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String retrieveLastElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void remove(String newElement, DoubleLinkedListDriver.StringComparator sComp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ListIterator<String> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ SortedDoubleLinkedList.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class SortedDoubleLinkedList<T> {
+
+	public SortedDoubleLinkedList(DoubleLinkedListDriver.StringComparator sComp) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void add(String newElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ArrayList<String> toArrayList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String retrieveFirstElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String retrieveLastElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void remove(String newElement, DoubleLinkedListDriver.StringComparator sComp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ListIterator<String> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
